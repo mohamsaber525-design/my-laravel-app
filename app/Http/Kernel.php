f2a0fbea607feda5660dea8bd5383562b26888e5
dell <wadii0638950242@gmail.com>
2025-12-03 17:11:52 +0100

    description of my changes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Http\Kernel as HttpKernel;
 class Kernel extends HttpKernel
 {
     protected $middleware = [
+        \App\Http\Middleware\Cors::class,
         \Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests::class,
         \Illuminate\Foundation\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
